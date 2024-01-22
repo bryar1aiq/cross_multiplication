dac5b78eb604d6f88fba13b3feec8922e4c38056
Bryar <bryar1.faiq@gmail.com>
2024-01-22 14:32:07 +0300

    Added the SVG images to the assets folder + Put one of them in the stack in the OnBoardingScreen.

---
+++ lib/views/colors.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+class Colors {
+  Color primaryTeal = const Color(0xFF00D3AD);
+}
--- lib/views/onboarding_screen.dart
+++ lib/views/onboarding_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+              top: 0,
+              left: 0,
+              child: SvgPicture.asset('assets/images/svg/eclipses.svg'))
+        ],
+      ),
+    );
   }
 }
